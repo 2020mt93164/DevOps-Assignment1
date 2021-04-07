8e9816507d8dd7eb2f30f2d2de718c325fe10fd0
Krupal Ghorpade <3656992+krupalghorpade@users.noreply.github.com>
2021-04-08 01:45:50 +0530

    Added welcome message

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld{
+	public static void main(String[] args) {
+		
+		HelloWorld instance = new HelloWorld();
+		instance.printWelcomeMessage();
+
+	}
+
+	private void printWelcomeMessage(){
+		System.out.println("Welcome to software installation.");
+	}
+}
