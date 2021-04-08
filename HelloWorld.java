773071ea890d10d6160341079f5b43cb258d490b
Rusia <Arpit.Rusia@capco.com>
2021-04-08 19:16:43 +0530

    added installation success message

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,10 +3,14 @@ public class HelloWorld{
 		
 		HelloWorld instance = new HelloWorld();
 		instance.printWelcomeMessage();
+                instance.printInstallationSuccessMessage();
 
 	}
 
 	private void printWelcomeMessage(){
 		System.out.println("Welcome to software installation.");
 	}
+        private void printInstallationSuccessMessage(){    // Arpit to do this
+	System.out.println("Checking your system");
+}
 }
